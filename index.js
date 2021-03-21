147532e9e354c547d7e7dd13e94a4df43e5ba24c
johnny <johnny1194@hotmail.com>
2021-03-21 02:55:03 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["johnny", "raul", "fernando"];
+const ocassion = "Surprise";
+
+function writeCards(names,ocassion) {
+  const messages = []
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${ocassion} gift!`;
+    messages.push(message);
+  }
+  return messages
+}
+
+writeCards[names];
+
+function countDown(number){
+    for (let i = number; i >= 0 ; i--){
+    console.log(i);
+    }
+}
